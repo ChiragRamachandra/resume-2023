8cec0d6e1be7d8ecf4c65ebe87b783f4c1a380ca
Chirag Ramachandra <chirag.ramachandra@gmail.com>
2023-01-20 20:49:09 -0500

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -23,7 +23,7 @@ const About = (props: Props) => {
 				viewport={{ once: true }}
 				transition={{ duration: 0.3 }}
 				className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]'
-				src='/images/chirag_ramachandra.jpg'
+				src='/images/profile/chirag_ramachandra.jpg'
 			/>
 			<div className='space-y-10 px-10 md:px-10'>
 				<h4 className='text-4xl font-semibold'>
