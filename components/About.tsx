d9dab5fc5f6da516125e3139ed7726272df72876
Chirag Ramachandra <chirag.ramachandra@gmail.com>
2023-02-23 15:40:48 +0530

    fix: about mobile

--- components/About.tsx
+++ components/About.tsx
@@ -12,30 +12,35 @@ const About = (props: Props) => {
 			transition={{ duration: 0.3 }}
 			className='h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl justify-evenly mx-auto items-center p-5'
 		>
-			<h3 className='uppercase absolute tracking-[20px] text-gray-500 text-2xl top-24'>
-				About
-			</h3>
+			<div className='my-6'>
+				<h3 className='uppercase tracking-[20px] text-gray-500 text-2xl '>
+					About
+				</h3>
+			</div>
 
-			<motion.img
-				loading='lazy'
-				initial={{ x: -200, opacity: 0 }}
-				whileInView={{ x: 0, opacity: 1 }}
-				viewport={{ once: true }}
-				transition={{ duration: 0.3 }}
-				className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]'
-				src='/images/profile/chirag_ramachandra.jpg'
-			/>
-			<div className='space-y-10 px-10 md:px-10'>
+			<div className='m-6 flex-shrink-0'>
+				<motion.img
+					loading='lazy'
+					initial={{ x: -200, opacity: 0 }}
+					whileInView={{ x: 0, opacity: 1 }}
+					viewport={{ once: true }}
+					transition={{ duration: 0.3 }}
+					className='-mb-15 -mt-10 md:mb-0 flex-shrink-0 w-40 h-40 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]'
+					src='/images/profile/chirag_ramachandra.jpg'
+				/>
+			</div>
+
+			<div className='space-y-10 px-4 md:px-10'>
 				<h4 className='text-4xl font-semibold'>
 					A <span className='text-[#1ecbe1]'>little</span> bit about me.
 				</h4>
-				<p className='text-sm'>
+				<p className='text-sm md:text-base text-justify'>
 					I am a Full Stack Developer, currently working with Getty Images. I
 					enjoy building things and bringing ideas to life. I am equipped with
 					the right skills to help you with your next project, let&apos;s create
 					together.
 				</p>
-				<p className='text-sm'>
+				<p className='text-sm md:text-base text-justify'>
 					I believe in working smart and not reinventing the wheel, which leads
 					me to work with a multitude of 3rd party tools/ APIs which are
 					integrated into the web application. While working, I tend to
